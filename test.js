7ce27888c14c0e0c0badeb84d6f1889636f584c4
codingkrishna <codingkrishna@gmail.com>
2023-11-27 07:39:45 +0530

    json

--- test.js
+++ test.js
@@ -12,12 +12,37 @@
 // console.log(r1);
 
 // JSON
-var user = {"id":12, "userName":"ravi", "email": "ravi@gmail.com", "mobile": ["12345", "789345"],
-"address": {
-    "location": "ohio",
-    "state": "ohio"
-}}
-console.log(user.address.state);
+// var user = {"id":12, "userName":"ravi", "email": "ravi@gmail.com", "mobile": ["12345", "789345"],
+// "address": {
+//     "location": "ohio",
+//     "state": "ohio"
+// }}
+// console.log(user.address.state);
 
-var msg = { "from":"890", "sender": "12345", "time": "", "sub":"hii", "body":"hii welcome"};
-console.log(msg);
+// var msg = { "from":"890", "sender": "12345", "time": "", "sub":"hii", "body":"hii welcome"};
+// console.log(msg);
+
+
+var users =[ {
+    "id": 123,
+    "name": "Ravi",
+    "email": "ravi@gmail.com",
+    "mobile": [1234, 8907],
+    "address" : {
+        "street": "20th main",
+        "city": "Blr"
+    }
+},
+{
+    "id": 345,
+    "name": "Shiva",
+    "email": "siva@gmail.com",
+    "mobile": [1234, 8907],
+    "address" : {
+        "street": "20th main",
+        "city": "Blr"
+    }
+}
+]
+
+console.log(users);
